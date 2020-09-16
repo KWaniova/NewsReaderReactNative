3cfcfcb9117ab0130d38e89750e9f01593df23af
KrystynaWaniova <krystyna.waniova@idevband.com>
2020-09-16 21:10:36 +0200

    Update rest_config.tsx

--- NewsReaderApp/src/config/rest_config.tsx
+++ NewsReaderApp/src/config/rest_config.tsx
@@ -3,6 +3,6 @@ import * as React from 'react';
 export const articles_url = 'http://newsapi.org/v2/top-headlines';
 export const country_code = 'cz';
 export const category = 'general';
-export const _api_key = 'd3757230ae6242d9b82e28123cea9dd8'
+export const _api_key = 'abc' //add API key for News API
 
 
